a747c93ad5ea8cd23cc88ae833e955d9e6d06900
Donald Plummer <donald@cideasphere.com>
2012-08-14 15:23:29 -0700

    Hack to safely migrate oauth2_clients

--- lib/oauth2/model/schema.rb
+++ lib/oauth2/model/schema.rb
@@ -3,32 +3,34 @@ module OAuth2
     
     class BaseSchema < ActiveRecord::Migration
       def change
-        create_table :oauth2_clients, :force => true do |t|
-          t.timestamps
-          t.string     :oauth2_client_owner_type
-          t.integer    :oauth2_client_owner_id
-          t.string     :name
-          t.string     :client_id
-          t.string     :client_secret_hash
-          t.string     :redirect_uri
+        unless ActiveRecord::Base.connection.tables.include?("oauth2_clients")
+          create_table :oauth2_clients do |t|
+            t.timestamps
+            t.string     :oauth2_client_owner_type
+            t.integer    :oauth2_client_owner_id
+            t.string     :name
+            t.string     :client_id
+            t.string     :client_secret_hash
+            t.string     :redirect_uri
+          end
+          add_index :oauth2_clients, :client_id
+          
+          create_table :oauth2_authorizations do |t|
+            t.timestamps
+            t.string     :oauth2_resource_owner_type
+            t.integer    :oauth2_resource_owner_id
+            t.belongs_to :client
+            t.string     :scope
+            t.string     :code,               :limit => 40
+            t.string     :access_token_hash,  :limit => 40
+            t.string     :refresh_token_hash, :limit => 40
+            t.datetime   :expires_at
+          end
+          add_index :oauth2_authorizations, [:client_id, :code]
+          add_index :oauth2_authorizations, [:access_token_hash]
+          add_index :oauth2_authorizations, [:client_id, :access_token_hash]
+          add_index :oauth2_authorizations, [:client_id, :refresh_token_hash]
         end
-        add_index :oauth2_clients, :client_id
-        
-        create_table :oauth2_authorizations, :force => true do |t|
-          t.timestamps
-          t.string     :oauth2_resource_owner_type
-          t.integer    :oauth2_resource_owner_id
-          t.belongs_to :client
-          t.string     :scope
-          t.string     :code,               :limit => 40
-          t.string     :access_token_hash,  :limit => 40
-          t.string     :refresh_token_hash, :limit => 40
-          t.datetime   :expires_at
-        end
-        add_index :oauth2_authorizations, [:client_id, :code]
-        add_index :oauth2_authorizations, [:access_token_hash]
-        add_index :oauth2_authorizations, [:client_id, :access_token_hash]
-        add_index :oauth2_authorizations, [:client_id, :refresh_token_hash]
       end
     end
 
@@ -43,7 +45,7 @@ module OAuth2
       MIGRATIONS = [BaseSchema, ClientTypeMigration]
 
       def self.up
-        MIGRATIONS.each {|m| m.migrate(:up)}
+        MIGRATIONS.each { |m| m.migrate(:up) }
       end
 
       def self.down
